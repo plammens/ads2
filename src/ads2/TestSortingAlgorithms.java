619fccedf14edc60240ea4716382bdb4c97c5e37
Paolo Lammens <lammenspaolo@gmail.com>
2020-02-12 23:17:43 +0000

    test: enhance TestSortingAlgorithms

--- src/ads2/TestSortingAlgorithms.java
+++ src/ads2/TestSortingAlgorithms.java
@@ -4,20 +4,29 @@ import static ads2.utils.Utils.*;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
-import ads2.sorters.Sorter;
-import ads2.sorters.ThreeWayQuickSort;
+import ads2.sorters.*;
 
 
 public class TestSortingAlgorithms {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Sorter sorter = new ThreeWayQuickSort();
+        List<Sorter> sorters = new ArrayList<>();
+//        sorters.add(new InsertionSort());
+        sorters.add(new QuickSort());
+        sorters.add(new CutoffQuickSort(10));
+        sorters.add(new MedianOfThreeQuickSort());
+        sorters.add(new ThreeWayQuickSort());
 
         for (String path : args) {
-            int[] arr = toIntArray(readIntList(new FileInputStream(path)));
-            sorter.sort(arr);
-            System.out.println(isSorted(arr));
+            for (Sorter sorter : sorters) {
+                int[] arr = toIntArray(readIntList(new FileInputStream(path)));
+                sorter.sort(arr);
+                System.out.print(sorter.getClass().getSimpleName() + ": ");
+                System.out.println(isSorted(arr));
+            }
         }
     }
 
